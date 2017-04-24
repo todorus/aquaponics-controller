bbb0a2751b9b7b1ca8325b43f962401821bf2b94
Theo van den Bogaart <todorus@todorus.com>
2017-04-24 21:30:31 +0200

    #2 listing networks in serial

--- controller/controller.ino
+++ controller/controller.ino
@@ -8,6 +8,16 @@ const char* SSID_NAME = "ESP8266WiFi";
 bool inConfigMode = false;
 bool wasInConfigMode = false;
 
+struct network {
+  String ssid;
+  int32_t rssi;
+  uint8_t encryptionType;
+};
+struct scan {
+  unsigned int ssidCount;
+  network * networks;
+};
+
 void setup() {
   Serial.begin(115200);
   pinMode(PIN_BUTTON, INPUT);
@@ -22,7 +32,6 @@ void loop(){
     if(inConfigMode){
       setupAp();
       printWifiStatus();
-      Serial.println("looking for client");
     }
     wasInConfigMode = inConfigMode;
   }
@@ -43,51 +52,61 @@ void setupAp()
 }
 
 void configMode(){
-  WiFiClient client = server.available(); 
-  Serial.print(".");
-  if (client)
-  {
-    // we have a new client sending some request
-    Serial.println("clientfigmode");
-    
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          client.print(prepareHtmlPage());
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } 
-        else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
+  scan scanResult = fetchNetworks();
+  listNetworks(scanResult);
+
+  Serial.println("looking for clients");
+  for(unsigned int x=0 ; x < 10000; x++){
+    WiFiClient client = server.available(); 
+    if(x % 1000 == 0)
+    {
+      Serial.print(".");
+    }
+    if (client)
+    {
+      // we have a new client sending some request
+      Serial.println("clientfigmode");
+      
+      // an http request ends with a blank line
+      boolean currentLineIsBlank = true;
+      while (client.connected()) {
+        if (client.available()) {
+          char c = client.read();
+          Serial.write(c);
+          // if you've gotten to the end of the line (received a newline
+          // character) and the line is blank, the http request has ended,
+          // so you can send a reply
+          if (c == '\n' && currentLineIsBlank) {
+            client.print(prepareHtmlPage(scanResult));
+          }
+          if (c == '\n') {
+            // you're starting a new line
+            currentLineIsBlank = true;
+          } 
+          else if (c != '\r') {
+            // you've gotten a character on the current line
+            currentLineIsBlank = false;
+          }
         }
       }
+      // give the web browser time to receive the data
+      delay(1);
+      
+      // close the connection:
+      client.stop();
+      Serial.println("client disonnected");
     }
-    // give the web browser time to receive the data
-    delay(1);
-    
-    // close the connection:
-    client.stop();
-    Serial.println("client disonnected");
   }
 }
 
-String prepareHtmlPage()
+String prepareHtmlPage(scan scanResult)
 {
+  String list = listNetworks(scanResult);
+  list += "</ul>";
   String htmlPage = 
      String("HTTP/1.1 200 OK\r\n") +
             "Content-Type: text/html\r\n" +
             "Connection: close\r\n" +  // the connection will be closed after completion of the response
-            "Refresh: 5\r\n" +  // refresh the page automatically every 5 sec
             "\r\n" +
             "<!DOCTYPE HTML>" +
             "<html>" +
@@ -96,6 +115,7 @@ String prepareHtmlPage()
             "\r\n";
   return htmlPage;
 }
+
 
 
 void printWifiStatus() {
@@ -113,4 +133,70 @@ void printWifiStatus() {
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
+}
+
+
+scan fetchNetworks() {
+  // scan for nearby networks:
+  scan result;
+  unsigned int ssidCount = WiFi.scanNetworks();
+  result.ssidCount = ssidCount;
+  result.networks = new network[result.ssidCount];
+  
+  for (int ssidNum = 0; ssidNum < ssidCount; ssidNum++) {
+    network nw;
+    nw.ssid = WiFi.SSID(ssidNum);
+    nw.rssi = WiFi.RSSI(ssidNum);
+    nw.encryptionType = WiFi.encryptionType(ssidNum);
+    result.networks[ssidNum] = nw;
+  }
+
+  return result;
+}
+
+String listNetworks(scan scanResult) {
+  String list = String("<ul>"); 
+  Serial.println();
+  for( unsigned int x = 0; x < scanResult.ssidCount; x++ ){
+    network nw = scanResult.networks[x];
+    list += "(";
+    list += x;
+    list +=") ";
+    list += nw.ssid;
+    list +="signal: ";
+    list += nw.rssi;
+    list += "dBm Encryption: ";
+    list += encryptionTypeToString(nw.encryptionType);
+    Serial.print(x);
+    Serial.print(") ");
+    Serial.print(nw.ssid);
+    Serial.print("\tSignal: ");
+    Serial.print(nw.rssi);
+    Serial.print(" dBm");
+    Serial.print("\tEncryption: ");
+    Serial.println(encryptionTypeToString(nw.encryptionType)); 
+  }
+  list += "</ul>";
+  return list;
+}
+
+String encryptionTypeToString(int thisType) {
+  // read the encryption type and print out the name:
+  switch (thisType) {
+    case ENC_TYPE_WEP:
+      return "WEP";
+      break;
+    case ENC_TYPE_TKIP:
+      return "WPA";
+      break;
+    case ENC_TYPE_CCMP:
+      return "WPA2";
+      break;
+    case ENC_TYPE_NONE:
+      return "None";
+      break;
+    case ENC_TYPE_AUTO:
+      return "Auto";
+      break;
+  }
 }
